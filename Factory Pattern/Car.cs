a4c4956b4f7e74c68d7b3a639da105920194a10a
Wade Wakefield <wadhwake@ut.utm.edu>
2018-02-06 15:48:13 -0600

    first commit

---
+++ Factory Pattern/Car.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factory_Pattern
+{
+    class Car
+    {
+        public string company;
+        public string model;
+        public int speed;
+
+        abstract private string get_company;
+        abstract private string get_model;
+        abstract private void set_model;
+        abstract private int get_speed;
+        abstract private void set_speed;
+        
+    }
+}
